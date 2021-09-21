1dc8b4d50740caf1326fcc5396666882d86d46aa
HaseebHassan <mhhassan095@gmail.com>
2021-09-21 11:28:48 -0700

    error class added

--- resources/views/web/default/cart/payment.blade.php
+++ resources/views/web/default/cart/payment.blade.php
@@ -54,7 +54,7 @@
                 <div class="panel-body col-xl-12">
 
 
-                    <div class="error "></div>
+                    <p class="error "></p>
 
 
                    <div class='form-row row'>
